7ad801f7fc555df6ca1008fdd7480eb33b7131a1
Petros Maliotis <maliotis12@gmail.com>
2020-04-10 13:49:32 +0100

    Petros changes

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -2,3 +2,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>Tripaki</title>
+<style>
+      /* Always set the map height explicitly to define the size of the div
+       * element that contains the map. */
+      #map {
+        height: 80%;
+      }
+      /* Optional: Makes the sample page fill the window. */
+      html, body {
+        height: 80%;
+        margin: 0;
+        padding: 0;
+      }
+</style>
